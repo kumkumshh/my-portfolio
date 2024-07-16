cc837ec9e3eab6a26d9508637af22022d2a7d894
kumkum sharma <kumkumsharma227@gmail.com>
2024-07-16 18:19:20 +0530

    commit

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,8 +1,11 @@
 import Work1 from "../../assets/work1.png";
 import Work2 from "../../assets/work2.png";
 import Work3 from "../../assets/work3.png";
-import Work4 from "../../assets/work4.jpg";
+import Work4 from "../../assets/work4.png";
 import Work5 from "../../assets/work5.png";
+import Work6 from "../../assets/work6.png";
+import Work7 from "../../assets/work7.png";
+
 
 export const projectsData = [
     {
@@ -10,31 +13,50 @@ export const projectsData = [
       image: Work1,
       title: "My Portfolio",
       category: "web",
+      url: "https://github.com/kumkumshh/my-portfolio",
     },
     {
       id: 2,
       image: Work2,
       title: "Movie Recommender System",
       category: "ml",
+      url: "https://moviefinderrr.streamlit.app/",
     },
     {
       id: 3,
       image: Work3,
       title: "Rock Paper Scissor",
       category: "web",
+      url: "https://github.com/kumkumshh/rock-paper-scissors",
     },
     {
       id: 4,
       image: Work4,
       title: "Car Price Prediction",
       category: "ml",
+      url: "https://github.com/kumkumshh/car-price-project",
     },
     {
       id: 5,
       image: Work5,
       title: "Static Food Website",
       category: "web",
+      url: "https://moviefinderrr.streamlit.app/",
     },
+    {
+      id: 6,
+      image: Work6,
+      title: "Real-Time ChatApp",
+      category: "web",
+      url: "https://github.com/kumkumshh/chatApp",
+    },
+    {
+      id: 7,
+      image: Work7,
+      title: "Fine Dine Restro",
+      category: "web",
+      url: "https://kumkumshh.github.io/fine-dine-restaurant-web/",
+    },   
   ];
 
   export const projectsNav = [
